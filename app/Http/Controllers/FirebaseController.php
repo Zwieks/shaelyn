5fea8a91c648f25f659a801333059a54c0cf50f8
Ronald Zwiers <ronald.zwiers@embracesbs.com>
2018-04-14 17:44:09 +0200

    Better formatting

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -11,11 +11,12 @@ use Illuminate\Http\Request;
 class FirebaseController extends Controller
 {
     public function index() {
-   	$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/shaelyn-487ff-firebase-adminsdk-4cyxu-819c761ef1.json');
+   		$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/shaelyn-487ff-firebase-adminsdk-4cyxu-819c761ef1.json');
 
-	$firebase = (new Factory)
-    	->withServiceAccount($serviceAccount)
-    	->create();
+		$firebase = (new Factory)
+	    	->withServiceAccount($serviceAccount)
+	    	->create();
+	    	
         $database = $firebase->getDatabase();
 
         $auth = $firebase->getAuth();
