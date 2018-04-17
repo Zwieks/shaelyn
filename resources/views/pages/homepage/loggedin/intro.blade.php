e85d7eab4a0064d1a1a18a15898ff4f099af52b3
Ronald Zwiers <ronald.zwiers@embracesbs.com>
2018-04-17 13:01:55 +0200

    Added id's back to the loggedin template

--- resources/views/pages/homepage/loggedin/intro.blade.php
+++ resources/views/pages/homepage/loggedin/intro.blade.php
@@ -3,12 +3,10 @@
 	<div class="particles-content-wrapper">
 
 		<figure>
-			<img src="{{$user['thumb_image']}}" />
+			<img  id="firebase-image" src="{{$user['thumb_image']}}" />
 		</figure>
 
-		<pre id="object"></pre>
-
-		<h2 id="js-username" class="logo-text">{{$user['name']}}</h2>	
+		<h2 id="firebase-username" class="logo-text">{{$user['name']}}</h2>	
 
 		<button type="button" class="btn btn-primary" id="js-logout">{{ Lang::get('buttons.login') }}</button>
 	</div>
