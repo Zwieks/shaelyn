66e125b924df25bfb5a950b3b296d2a5d5970621
Ronald Zwiers <ronald.zwiers@embracesbs.com>
2018-05-01 23:11:32 +0200

    Added user initials functionality

--- public/js/jquery.acties.js
+++ public/js/jquery.acties.js
@@ -26,6 +26,13 @@ jQuery(window).on('load', function(){
 
 });
 
+function getInitials(name) {
+	var initials = name.match(/\b\w/g) || [];
+		initials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+	
+	return initials;
+}
+
 function setEndOfContenteditable(contentEditableElement)
 {
     var range,selection;
